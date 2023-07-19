e7c8a33a18827247572ed1345e8873871933192d
Anna Galkevich <emikot321@gmail.com>
2023-07-19 22:39:23 +0300

    Added header and hero

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header/Header";
+import Hero from "./components/hero/Hero";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Hero.css";
+import { HiLocationMarker } from "react-icons/hi";
+
+const Hero = () => {
+  return (
+    <section className="hero-wrapper">
+      <div className="paddings innerWidth flexCenter hero-container">
+        {/* left side */}
+        <div className="flexColStart hero-left">
+          <div className="orange-circle" />
+          <div className="hero-title">
+            <h1>
+              Discover <br /> Most Suitable <br /> Property
+            </h1>
+          </div>
+          <div className="flexColStart hero-des">
+            <span>Find a variety of properties that suit you very easilty</span>
+            <span>Forget all difficulties in finding a residence for you</span>
+          </div>
+          <div className="flexCenter search-bar">
+            <HiLocationMarker color="var(--blue)" size={25} />
+            <input type="text" />
+            <button className="button">Search</button>
+          </div>
+          <div className="flexCenter stats">
+            <div className="flexColStart stat">
+                <span></span>
+                <span></span>
+            </div>
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="flexCenter hero-right">
+          <div className="image-container">
+            <img src="./hero-image.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
