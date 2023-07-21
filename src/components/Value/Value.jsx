36269b1ee64b689e203f50d307676f92b1685d05
Anna Galkevich <emikot321@gmail.com>
2023-07-22 00:22:45 +0300

    Added library Accordion

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import React from "react";
 import {
-    Accordion,
-    AccordionItem,
-    AccordionItemHeading,
-    AccordionItemButton,
-    AccordionItemPanel,
-    AccordionItemState
-} from 'react-accessible-accordion'
-import 'react-accessible-accordion/dist/fancy-example.css';
-import {MdOutlineArrowDropDown} from 'react-icons/md';
-import './Value.css';
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+  AccordionItemState,
+} from "react-accessible-accordion";
+import "react-accessible-accordion/dist/fancy-example.css";
+import { MdOutlineArrowDropDown } from "react-icons/md";
+import "./Value.css";
 
 const Value = () => {
-    return (
-        <section className="v-wrapper">
-            <div className="paddings innerWidth flexCenter v-container">
-                {/* left side */}
-                <div className="v-left">
-                    <div className="image-container">
-                        <img src="./value.png" alt="" />
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="v-wrapper">
+      <div className="paddings innerWidth flexCenter v-container">
+        {/* left side */}
+        <div className="v-left">
+          <div className="image-container">
+            <img src="./value.png" alt="" />
+          </div>
+        </div>
+        {/* left side */}
+        <div className="flexColStart v-right">
+          <span className="orangeText">Our Value</span>
+          <span className="primaryText">Value We Give to You</span>
+          <span className="secondaryText">
+            We always ready to help by providing the best services for you.
+            <br />
+            We believe a good blace to live can make your life better
+          </span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Value;
