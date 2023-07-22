5b7413f5ef5f2f948cf69961f044107364fc2ca2
Anna Galkevich <emikot321@gmail.com>
2023-07-22 23:49:45 +0300

    added getstarted

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Companies from "./components/Companies/Companies";
 import Residencies from "./components/Residencies/Residencies";
 import Value from "./components/Value/Value";
 import Contact from "./components/Contact/Contact";
+import GetStarted from "./components/GetStarted/GetStarted";
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
       <Residencies />
       <Value />
       <Contact/>
+      <GetStarted/>
+      <Footer/>
     </div>
   );
 }
